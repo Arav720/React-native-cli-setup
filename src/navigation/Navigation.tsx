a746ecb6a16fedf624696a7ed588d0f6848d9a45
Arav720 <134154390+Arav720@users.noreply.github.com>
2025-07-01 13:10:55 +0530

    Setup Tailwind with NativeWind, added navigation and screens

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SplashScreen from '../screens/SplashScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation: FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SplashScreen"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
